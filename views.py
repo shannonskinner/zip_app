364394328173da22a7d73c5b99bfd9b42b313933
Shannon Skinner <shannonskinner@Shannons-MacBook-Pro.local>
2018-12-10 12:59:28 -0600

    initial commit

---
+++ views.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, Response, session, redirect, url_for, escape, request, send_from_directory, jsonify, make_response
+from flask_bootstrap import Bootstrap
+import json
+import zip_code_lookup
+
+
+app = Flask(__name__)
+Bootstrap(app)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload():
+    '''try: 
+        zipcode = request.args['zipcode']
+    except KeyError:
+        zipcode = "No zipcode set"
+    try: 
+        county_output = request.args['county_output']
+    except KeyError:
+        county_output = "No zipcode set"
+    try: 
+        state_output = request.args['state_output']
+    except KeyError:
+        state_output = "No zipcode set"'''
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'zipcode' not in request.form:
+            flash('No zipcode requested')
+            return redirect(request.url)
+        zipcode = request.form['zipcode']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if zipcode:
+            response=zip_code_lookup.main(zipcode)
+            return redirect(url_for('script', zipcode=zipcode))
+    else: 
+        with open('output.json') as f: 
+            data = json.load(f)
+        zipcode=data[0]['zipcode']
+        county_output = data[0]['county_output']
+        state_output = data[0]['state_output']
+        city_output = data[0]['city_output']
+        #output = [{'zipcode': 'ENTER ZIPCODE', 'county_output': 'ENTER ZIPCODE', 'state_output': 'ENTER ZIPCODE', 'city_output': 'ENTER ZIPCODE'}]
+        #with open('output.json', 'w') as f:
+        #    json.dump(output, f)
+    return render_template('zip_app.html', zipcode=zipcode, county_output=county_output, state_output=state_output, city_output=city_output)
+
+@app.route('/run_script')
+def script(): 
+    zipcode = request.args['zipcode']
+    response=zip_code_lookup.main(zipcode)
+    return redirect(url_for('upload'))
---
+++ zip_code_lookup.py
@@ -0,0 +1,40 @@
+
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+import json
+
+
+# bring in data from Census sources
+def main(zipcode):
+    input_zip=zipcode
+    zips = pd.read_csv('https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_county_rel_10.txt', dtype={'ZCTA5': str, 'STATE': str, 'COUNTY':str, 'GEOID':str}) #read census data for zipcodes & state / county
+    
+    counties = pd.read_csv('https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt', header=None, names=['STATE','STATEFP', 'COUNTYFP', 'COUNTYNAME', 'CLASSFP'], dtype={'STATEFP': str, 'COUNTYFP':str, 'GEOID':str}) #read list of counties
+    
+    place_zip = pd.read_csv('https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_place_rel_10.txt', dtype={'ZCTA5': str, 'STATE': str, 'PLACE':str, 'GEOID':str})
+    places = pd.read_csv('https://www2.census.gov/geo/docs/reference/codes/files/national_places.txt', 
+                     encoding='latin-1', sep='|', 
+                     header=0, names=['STATE','STATEFP', 'PLACEFP', 'PLACENAME', 'TYPE', 'FUNCSTAT', 'COUNTY'], 
+                     dtype={'STATEFP': str, 'PLACEFP':str})#import places list
+    
+    
+    counties['GEOID'] = counties.STATEFP.map(str)+counties.COUNTYFP.map(str)
+    counties = counties[['STATE', 'GEOID', 'STATEFP', 'COUNTYFP', 'COUNTYNAME', 'CLASSFP']]
+    
+    input_geoid = str(zips[zips['ZCTA5']==input_zip]['GEOID'].values[0])
+    county_output = counties.COUNTYNAME[counties.GEOID==str(zips[zips['ZCTA5']==input_zip]['GEOID'].values[0])].values[0]
+    state_output = counties.STATE[counties.GEOID==str(zips[zips['ZCTA5']==input_zip]['GEOID'].values[0])].values[0]
+    place_id = place_zip[place_zip['ZCTA5']==zipcode]['PLACE'].values[0]
+    city_output = places[(places['PLACEFP']==place_id)& (places['STATE']==state_output)]['PLACENAME'].values[0]
+    
+    output = [{'zipcode': zipcode, 'county_output': county_output, 'state_output': state_output, 'city_output': city_output}]
+    with open('output.json', 'w') as f:
+        json.dump(output, f)
+    
+
+    #print('Zip code ',input_zip, ' is in ',county_output,', ',state_output,'. Thank you!')
+
+if __name__=="__main__":
+    main()
