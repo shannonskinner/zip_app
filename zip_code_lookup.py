06b0c5c5c173e5b9a767e6ffb16cdc08bf4386cd
Shannon Slepak-Cherney <shannonskinner@gmail.com>
2018-12-11 10:30:04 -0600

    add user agent update

--- zip_code_lookup.py
+++ zip_code_lookup.py
@@ -3,32 +3,30 @@
 
 import pandas as pd
 import numpy as np
-from urllib.request import Request, urlopen
-from fake_useragent import UserAgent
-import urllib.request
+from urllib3.request import Request, urlopen
+#from fake_useragent import UserAgent
+import urllib3.request
 import json
+import io
 
 
 # bring in data from Census sources
 def main(zipcode):
     input_zip=zipcode
     header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-    ua=UserAgent()
+    #ua=UserAgent()
     
-    zip_req = Request('https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_county_rel_10.txt')    
-    zip_req.add_header('User-Agent', ua.chrome)
-    zip_req2 = urlopen(zip_req)
-    zips = pd.read_csv(zip_req2,  dtype={'ZCTA5': str, 'STATE': str, 'COUNTY':str, 'GEOID':str}) #read census data for zipcodes & state / county
+    zip_url = 'https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_county_rel_10.txt'  
+    zip_req = Request (url=zip_url, headers=header)
+    zips = pd.read_csv(io.StringIO(zip_req.decode('utf-8')),  dtype={'ZCTA5': str, 'STATE': str, 'COUNTY':str, 'GEOID':str}) #read census data for zipcodes & state / county
     
-    count_req = Request('https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt')    
-    count_req.add_header('User-Agent', ua.chrome)
-    count_req2 = urlopen(count_req)
-    counties = pd.read_csv(count_req2, header=None, names=['STATE','STATEFP', 'COUNTYFP', 'COUNTYNAME', 'CLASSFP'], dtype={'STATEFP': str, 'COUNTYFP':str, 'GEOID':str}) #read list of counties
+    count_url = 'https://www2.census.gov/geo/docs/reference/codes/files/national_county.txt' 
+    count_req = Request(url=count_url, headers=header)
+    counties = pd.read_csv(io.StringIO(count_req.decode('utf-8')), header=None, names=['STATE','STATEFP', 'COUNTYFP', 'COUNTYNAME', 'CLASSFP'], dtype={'STATEFP': str, 'COUNTYFP':str, 'GEOID':str}) #read list of counties
     
-    place_req = Request('https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_place_rel_10.txt', headers=header )    
-    place_req.add_header('User-Agent', ua.chrome)
-    place_req2 = urlopen(place_req)
-    place_zip = pd.read_csv(place_req2, dtype={'ZCTA5': str, 'STATE': str, 'PLACE':str, 'GEOID':str})
+    place_url = 'https://www2.census.gov/geo/docs/maps-data/data/rel/zcta_place_rel_10.txt'
+    place_req = Request(url=place_url, headers=header)
+    place_zip = pd.read_csv(io.StringIO(place_req.decode('utf-8')), dtype={'ZCTA5': str, 'STATE': str, 'PLACE':str, 'GEOID':str})
     places = pd.read_csv('https://www2.census.gov/geo/docs/reference/codes/files/national_places.txt', 
                      encoding='latin-1', sep='|', 
                      header=0, names=['STATE','STATEFP', 'PLACEFP', 'PLACENAME', 'TYPE', 'FUNCSTAT', 'COUNTY'], 
